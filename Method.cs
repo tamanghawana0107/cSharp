375701f04db39de208fef40728cf8e5debd29b34
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-08 23:37:02 +0545

    New commit

---
+++ Method.cs
@@ -0,0 +1,13 @@
+using System;
+namespace cSharp
+{
+    class Method
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write Method");
+            Console.Write("Easy Method");
+            Console.Write("\n It prints on the same line");
+        }
+    }
+}
