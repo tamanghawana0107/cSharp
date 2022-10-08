e3fcaea21e12e8e62d8d8abdb3631dc1ae6ca264
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-08 23:12:57 +0545

    https://github.com/tamanghawana0107/cSharp.git

---
+++ firstDay.cs
@@ -0,0 +1,14 @@
+
+using System;
+namespace HelloWorld
+{
+    class firstDay
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello everyone!");
+            Console.WriteLine("First Day of learning C#!");
+            Console.WriteLine("Feeling Excited");
+        }
+    }
+}
