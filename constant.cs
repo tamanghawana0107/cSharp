346c53231d15fc57bd517dd1c1cbcf4b3ffa6161
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-09 22:53:02 +0545

    added

---
+++ constant.cs
@@ -0,0 +1,15 @@
+//constant
+using System;
+namespace cSharp
+{
+    class constant
+    {
+        static void Main(string[] args)
+        {
+            const int myNum=0107;
+            const string name = "Riti Shrestha";
+            Console.WriteLine(myNum);
+            Console.WriteLine(name);
+        }
+    }
+}
