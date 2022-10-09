875d7c3fe308c2cee254da1de5e1933d66fdaa53
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-09 15:33:11 +0545

    New Commit

---
+++ variables.cs
@@ -0,0 +1,23 @@
+using System;
+namespace cSharp
+{
+    class variables
+    {
+        static void Main(String[] args)
+        {
+            //variablesDeclaration
+            string name ="Riti Shrestha";
+            int myNum;
+            myNum = 20;
+            double doubleNum=2.33D;
+            char letter= 'R';
+            bool boollean = false;
+            //variableImplementation
+            Console.WriteLine(name);
+            Console.WriteLine(myNum);
+            Console.WriteLine(doubleNum);
+            Console.WriteLine(letter);
+            Console.WriteLine(boollean);
+        }
+    }
+}
