0ef92a79eef491224d29deb74eb34ff1176b1034
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-12 12:39:46 +0545

    multiple variable created

---
+++ multipleVariable.cs
@@ -0,0 +1,17 @@
+//multiple variable
+using System;
+namespace cSharp
+{
+    class multipleVariable
+    {
+        static void Main(string[] args)
+        {
+            int x=5,y=6,z=50;
+            Console.WriteLine(x*y*z);
+            int a,b,c;
+            a=b=20;
+            c=30;
+            Console.WriteLine(a+b+c);
+        }
+    }
+}
