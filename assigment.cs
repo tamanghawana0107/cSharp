60149b0e0db17488ee5b60f05e50fc9f058517e3
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-17 17:23:49 +0545

    Assignemtn operator program

---
+++ assigment.cs
@@ -0,0 +1,14 @@
+//assignment operator
+using System;
+namespace cSharp
+{
+    class assignment
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine("x="+x);
+            Console.WriteLine("x="+(x+3));
+        }
+    }
+}
