e03e1b4692cec01c0e9b53b2065325b8bfffd0be
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-21 22:15:11 +0545

    Access of Character

---
+++ Access.cs
@@ -0,0 +1,17 @@
+//Access of characters in string
+using System;
+namespace cSharp
+{
+    class Access
+    {
+        static void Main(string[] args)
+        {
+            string college = "Kist College";
+            Console.WriteLine(college[0]);  //character
+            int charPos = college.IndexOf("C");
+            Console.WriteLine(charPos);  //position of character
+            string lastName = college.Substring(charPos);
+            Console.WriteLine(lastName);
+        }
+    }
+}
