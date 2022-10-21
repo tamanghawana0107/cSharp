72c407eccb9242e1424a721514d224513a2ebf10
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-21 21:50:44 +0545

    startup project added

---
+++ conca program.cs
@@ -0,0 +1,18 @@
+//string concatination
+using System;
+namespace cSharp
+{
+    class conca
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Romiya";
+            string lastName = "Dangol";
+            //using manual method
+            string name1 = firstName  + lastName;
+            Console.WriteLine(name1);
+            //using concatination method
+            string name2
+        }
+    }
+}
