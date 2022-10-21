c735d4090aa32b266d70f51635a80e26b89366bd
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-21 21:43:43 +0545

    method of string added

--- cSharp_strings.cs
+++ cSharp_strings.cs
@@ -12,6 +12,9 @@ namespace cSharp
             //printing text length
             Console.WriteLine("The Length of the name string is :" + name.Length);
             //alternative printing way
+            string name2 = "Romiya Dangol";
+            Console.WriteLine(name2.ToUpper());
+            Console.WriteLine(name2.ToLower());
         }
     }
 }
