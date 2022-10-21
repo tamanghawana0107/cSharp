6e7aec6c5b78bf159a20a13b9628bf8938667c1a
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-21 21:41:48 +0545

    string program

---
+++ cSharp_strings.cs
@@ -0,0 +1,17 @@
+//string in C#
+using System;
+namespace cSharp
+{
+    class cSharp_strings
+    {
+        static void Main(string[] args)
+        {
+            //printing text length
+            string name = "Hawana Tamang";
+            Console.WriteLine(name);
+            //printing text length
+            Console.WriteLine("The Length of the name string is :" + name.Length);
+            //alternative printing way
+        }
+    }
+}
