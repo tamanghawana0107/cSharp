a7cec6c165e7089354c8e739ec7297d7413350c4
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-22 22:45:03 +0545

    Boolean program Added

---
+++ boolean_program.cs
@@ -0,0 +1,16 @@
+//C# boolean 
+using System;
+namespace cSharp
+{
+    class boolean_program
+    {
+        static void Main(string[] args)
+        {
+            bool isCodingFun = true;
+            bool isPumpkinTasty = false;
+            Console.WriteLine(isCodingFun);
+            Console.WriteLine(isPumpkinTasty);
+            
+        }
+    }
+}
