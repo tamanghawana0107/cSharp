15e91b144edea0eb03ce9c5c48dd6adced30f276
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-23 23:52:41 +0545

    for...loop program

---
+++ program_forLoop.cs
@@ -0,0 +1,15 @@
+//for...loop Program
+using System;
+namespace cSharp
+{
+    class program_forLoop
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1;i<13;i++)
+            {
+                Console.WriteLine("Romiya Dangol studies in Kist College");
+            }
+        }
+    }
+}
