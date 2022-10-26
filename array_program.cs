201f307af4fbe29e621a28dbe646a508bd89e32d
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-26 22:48:25 +0545

    Updated array code

--- array_program.cs
+++ array_program.cs
@@ -6,8 +6,13 @@ namespace cSharp
     {
         static void Main(string[] args)
         {
-            string[] name ={"Hawana","Romiya","Srijana","Sama"};
+            string[] name, address;
+            name = new string [] {"Hawana","Romiya","Srijana","Sama"};
+            address = new string [] {"Gathaghar", "Manamaiju","Lubhu"};
             Console.WriteLine(name[1]);
+            Console.WriteLine(address[1]);
+            Console.WriteLine(name.Length);
+            Console.WriteLine(address.Length);
         }
     }
 }
