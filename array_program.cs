4a4e68743c6eaf9e3f9894e95ccafaba4e6aa758
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-26 22:39:36 +0545

    Array program added

---
+++ array_program.cs
@@ -0,0 +1,13 @@
+//array in C#
+using System;
+namespace cSharp
+{
+    class array_program
+    {
+        static void Main(string[] args)
+        {
+            string[] name ={"Hawana","Romiya","Srijana","Sama"};
+            Console.WriteLine(name[1]);
+        }
+    }
+}
