e49ad0003b18f495c0d3fa9922bde07af88c4e81
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-28 23:13:43 +0545

    program added

---
+++ Sort.cs
@@ -0,0 +1,16 @@
+//System.Linq Namespace
+using System;
+using System.Linq;
+namespace cSharp
+{
+    class Sort
+    {
+        static void Main(string[] args)
+        {
+            int[] myNum = {5,13,09,08,15};
+            Console.WriteLine(myNum.Max());
+            Console.WriteLine(myNum.Min());
+            Console.WriteLine(myNum.Sum());
+        }
+    }
+}
