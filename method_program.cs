a7daaf0fcfd6d4b91ec34e66628be6b30739124a
tamanghawana0107 <hawanatamang@gmail.com>
2022-10-30 22:49:48 +0545

    added program

---
+++ method_program.cs
@@ -0,0 +1,16 @@
+//function method in C# 
+using System;
+namespace cSharp
+{
+    class method_program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
