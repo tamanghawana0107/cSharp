39dd8847b75527a2db4f7aea62563aad091777df
tamanghawana0107 <hawanatamang@gmail.com>
2022-11-01 00:05:48 +0545

    object added

---
+++ method3.cs
@@ -0,0 +1,16 @@
+//multiple parameters
+using System;
+namespace cSharp
+{
+    class method3
+    {
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + "is" + age);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Romiya",16);
+        }
+    }
+}
