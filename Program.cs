24d4f4662a8b66f204afd1d3cbe7877b82d969ce
Hawana Tamang <91280245+tamanghawana0815@users.noreply.github.com>
2022-11-24 09:25:47 +0545

    done in college

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
-namespace cSharp
+namespace Calculator
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Hello Riti Shrestha");
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
 }
